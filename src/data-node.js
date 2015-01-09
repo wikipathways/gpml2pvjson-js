b493037bd43414e24c2ed191ade8f97c682c94b1
Anders Riutta <git@andersriutta.com>
2015-01-09 13:23:48 -0800

    Update references to bridgedbjs

--- src/data-node.js
+++ src/data-node.js
@@ -1,11 +1,10 @@
 'use strict';
 
-var GpmlElement = require('./element.js')
-  , Graphics = require('./graphics.js')
-  , fs = require('fs')
-  , BridgeDb = require('bridgedbjs')
-  , BridgeDbDataSources = require('./data-sources.json')
-  ;
+var GpmlElement = require('./element.js');
+var Graphics = require('./graphics.js');
+var fs = require('fs');
+//var BridgeDb = require('bridgedb');
+var BridgeDbDataSources = require('./data-sources.json');
 
 //var BridgeDbDataSources = JSON.parse(fs.readFileSync('../data-sources.json'));
 
@@ -21,18 +20,21 @@ module.exports = {
     'Pathway':'Pathway',
     'Complex':'Complex'
   },
-  generateEntityReference: function(displayName, dataSourceName, dbId, organism, entityType, callback){
-    var bridgeDbDataSourcesRow
-      , bridgeDbDbNameCode
-      , entityReference = {}
-      , entityReferenceType
-      ;
+  generateEntityReference: function(
+      displayName, dataSourceName, dbId, organism, entityType, callback) {
+    var bridgeDbDataSourcesRow;
+    var bridgeDbDbNameCode;
+    var entityReference = {};
+    var entityReferenceType;
 
     entityReference.displayName = displayName;
     entityReference.type = entityType + 'Reference';
     // get external database namespace (as specified at identifiers.org) from GPML Xref Database attribute value.
     bridgeDbDataSourcesRow = BridgeDbDataSources.filter(function(dataSource) {
-      return dataSource.dataSourceName.toLowerCase().replace(/[^a-z0-9]/gi,'') === dataSourceName.toLowerCase().replace(/[^a-z0-9]/gi,'');
+      return dataSource.dataSourceName.toLowerCase()
+      .replace(/[^a-z0-9]/gi, '') ===
+          dataSourceName.toLowerCase()
+          .replace(/[^a-z0-9]/gi, '');
     })[0];
     if (!!bridgeDbDataSourcesRow) {
       var dbName = bridgeDbDataSourcesRow.namespace;
@@ -44,29 +46,33 @@ module.exports = {
       if (!!organism && !!bridgeDbDbNameCode && !!dbName && !!dbId) {
         // This URL is what BridgeDB currently uses. Note it currently returns TSV.
         // It would be nice to change the URL to something like the second version below. It would also be nice to return JSON-LD.
-        entityReference.xrefs = [encodeURI('http://webservice.bridgedb.org/' + organism + '/xrefs/' + bridgeDbDbNameCode + '/' + dbId)];
+        entityReference.xrefs = [encodeURI('http://webservice.bridgedb.org/' +
+            organism + '/xrefs/' + bridgeDbDbNameCode + '/' + dbId)];
 
         /*
            entityReference.xrefs = encodeURI('http://bridgedb.org/' + dbName + '/' + dbId + '/xref');
         //*/
 
         if (dbName === 'ensembl' || dbName === 'ncbigene') {
-          entityReference.xrefs.push(encodeURI('http://mygene.info/v2/gene/' + dbId));
+          entityReference.xrefs.push(
+              encodeURI('http://mygene.info/v2/gene/' + dbId));
         }
       }
       callback(null, entityReference);
     } else {
-      console.warn('Cannot find specified external reference database in the BridgeBD data-sources.txt file.');
-      callback('Cannot find specified external reference database in the BridgeBD data-sources.txt file.', null);
+      var message = 'Cannot find specified external reference database ' +
+          'in the BridgeBD data-sources.txt file.';
+      console.log(message);
+      callback(message, null);
     }
   },
-  toPvjson: function(pathway, gpmlSelection, dataNodeSelection, callbackInside) {
-    var generateEntityReference = this.generateEntityReference
-      , organism = pathway.organism
-      , pvjsonElements
-      , entity = {}
-      , gpmlDataNodeType = dataNodeSelection.attr('Type')
-      ;
+  toPvjson: function(
+      pathway, gpmlSelection, dataNodeSelection, callbackInside) {
+    var generateEntityReference = this.generateEntityReference;
+    var organism = pathway.organism;
+    var pvjsonElements;
+    var entity = {};
+    var gpmlDataNodeType = dataNodeSelection.attr('Type');
 
     if (!gpmlDataNodeType) {
       gpmlDataNodeType = 'Unknown';
@@ -78,24 +84,29 @@ module.exports = {
       entity.type = entityType;
     }
 
-    GpmlElement.toPvjson(pathway, gpmlSelection, dataNodeSelection, entity, function(entity) {
-      Graphics.toPvjson(pathway, gpmlSelection, dataNodeSelection, entity, function(entity) {
-        var entityReferences = [entity.id]
-          , dataSourceName
-          , dbId
-          , userSpecifiedXref
-          , xrefSelection = dataNodeSelection.find('Xref').eq(0)
-          ;
+    GpmlElement.toPvjson(
+        pathway, gpmlSelection, dataNodeSelection, entity, function(entity) {
+      Graphics.toPvjson(
+          pathway, gpmlSelection, dataNodeSelection, entity, function(entity) {
+        var entityReferences = [entity.id];
+        var dataSourceName;
+        var dbId;
+        var userSpecifiedXref;
+        var xrefSelection = dataNodeSelection.find('Xref').eq(0);
+
         if (xrefSelection.length > 0) {
           dataSourceName = xrefSelection.attr('Database');
           dbId = xrefSelection.attr('ID');
           if (!!dataSourceName && !!dbId) {
-            generateEntityReference(entity.textContent, dataSourceName, dbId, organism, entityType, function(err, entityReference) {
+            generateEntityReference(
+                entity.textContent, dataSourceName, dbId, organism, entityType,
+                function(err, entityReference) {
               if (!!entityReference) {
                 var entityReferenceId = entityReference.id;
                 entity.entityReference = entityReferenceId;
 
-                var entityReferenceExists = pathway.elements.filter(function(entity) {
+                var entityReferenceExists = pathway.elements
+                .filter(function(entity) {
                   return entity.id === entityReferenceId;
                 }).length > 0;
 
@@ -103,7 +114,7 @@ module.exports = {
                 if (entityType === 'Pathway' || !!entityReferenceExists) {
                   if (entityType === 'Pathway') {
                     entity.organism = organism;
-                  } 
+                  }
                   pvjsonElements = [entity];
                 } else {
                   pvjsonElements = [entity, entityReference];
