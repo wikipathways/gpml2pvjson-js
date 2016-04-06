5f9cceb8ad032a4ae4d84a8092cf13ed904fef6f
Anders Riutta <git@andersriutta.com>
2016-04-05 22:52:53 -0700

    Attempting to make tests work

--- test/spec/api.js
+++ test/spec/api.js
@@ -2,10 +2,9 @@
  * Test public APIs
  */
 
-//var _ = require('lodash');
 var expect = require('chai').expect;
-var Gpml2Pvjson = require('../../index.js');
-//var RxNode = require('../../index.js');
+var fs = require('fs');
+var jsdom = require('mocha-jsdom');
 var sinon = require('sinon');
 var sologger = require('../sologger.js');
 
@@ -13,6 +12,53 @@ var sologger = require('../sologger.js');
 
 // Run tests
 describe('Public API', function() {
+  var $;
+  var Gpml2Pvjson;
+  var parsedGpml;
+
+  //jsdom();
+
+//  before(function(done) {
+//    jsdom.env({
+//      file: __dirname + '/../input-data/WP554_84372.gpml',
+//      parsingMode: 'xml',
+//      done: function(err, window) {
+//        if (err) {
+//          return done(err);
+//        }
+//        parsedGpml = window.document.querySelector('Pathway');
+//        //console.log('parsedGpml');
+//        //console.log(parsedGpml);
+//        done();
+//      }
+//    });
+//    Gpml2Pvjson = require('../../index.js') || window.Gpml2Pvjson;
+//    //$ = require('jquery');
+//  });
+
+//  before(function() {
+//    Gpml2Pvjson = require('../../index.js') || window.Gpml2Pvjson;
+//    $ = require('jquery');
+//    var inputGpml = fs.readFileSync(__dirname + '/../input-data/WP554_84372.gpml', 'utf8');
+//    parsedGpml = $(inputGpml);
+//  });
+
+  before(function() {
+    //Gpml2Pvjson = require('../../index.js') || window.Gpml2Pvjson;
+    $ = require('jquery');
+    var inputGpml = fs.readFileSync(__dirname + '/../input-data/WP554_84372.gpml', 'utf8');
+    jsdom({
+      file: __dirname + '/../input-data/WP554_84372.gpml',
+      parsingMode: 'xml',
+      src: [
+        fs.readFileSync(__dirname + '/../../node_modules/jquery/src/jquery.js', 'utf-8'),
+        fs.readFileSync(__dirname + '/../../index.js', 'utf-8')
+      ]
+    });
+    Gpml2Pvjson = require('../../index.js');
+    //Gpml2Pvjson = require('../../index.js') || window.Gpml2Pvjson;
+    //parsedGpml = $(inputGpml);
+  });
 
   it('should create instance', function() {
     var gpml2PvjsonInstance = new Gpml2Pvjson();
@@ -20,26 +66,49 @@ describe('Public API', function() {
     expect(gpml2PvjsonInstance).to.respondTo('toPvjson');
   });
 
-//  describe('thenable (Promise)', function() {
-//    it('should work on success', function(done) {
-//      Rx.Observable.range(1, 3)
-//      .then(function(result) {
-//        expect(result).to.eql([1, 2, 3]);
-//        done();
-//      }, done);
-//    });
-//
-//    it('should work on error', function(done) {
-//      var message = 'placeholder error';
-//      Rx.Observable.range(1, 3)
-//      .concat(Rx.Observable.throw(new Error(message)))
-//      .then(function(result) {
-//        done(new Error('expected onError to be called, not onNext'));
-//      }, function(err) {
-//        expect(err.message).to.eql(message);
-//        done();
-//      });
+  it('should convert WP554 (streaming)', function(done) {
+    var inputGpml = fs.readFileSync(__dirname + '/../input-data/WP554_84372.gpml', 'utf8');
+    console.log(inputGpml);
+    var gpml2PvjsonInstance = new Gpml2Pvjson();
+    gpml2PvjsonInstance.toPvjsonSource(inputGpml, {
+      '@id': 'http://identifiers.org/wikipathways/WP554',
+      version: 0
+    })
+    .subscribe(function(pvjson) {
+      console.log('pvjson');
+      console.log(pvjson);
+      done();
+    }, done);
+  });
+
+//  it('should convert WP554', function(done) {
+//    var gpml2PvjsonInstance = new Gpml2Pvjson();
+//    gpml2PvjsonInstance.toPvjson(parsedGpml, {
+//      '@id': 'http://identifiers.org/wikipathways/WP554',
+//      version: 0
+//    }, function(err, pvjson) {
+//      if (err) {
+//        throw err;
+//      }
+//      console.log('pvjson');
+//      console.log(pvjson);
+//      done();
 //    });
 //  });
+
+  it('should convert WP554', function(done) {
+    var gpml2PvjsonInstance = new Gpml2Pvjson();
+    gpml2PvjsonInstance.toPvjson(parsedGpml, {
+      '@id': 'http://identifiers.org/wikipathways/WP554',
+      version: 0
+    }, function(err, pvjson) {
+      if (err) {
+        throw err;
+      }
+      console.log('pvjson');
+      console.log(pvjson);
+      done();
+    });
+  });
 
 });
